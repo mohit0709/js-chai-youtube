6f051ecdf0c0ca7c8d1cb8f15310995fe2c4d579
mohit-aglawe <154004573+mohit0709@users.noreply.github.com>
2024-04-09 09:37:19 +0000

    datatypes & variables

---
+++ js_basics/datatypes.js
@@ -0,0 +1,21 @@
+"strict mode" // treat all js code in strict mode
+
+// alert(3 + 3) we cannot use alert like this in node.js
+
+let name = "Mohit";
+let age = 18;
+let state = true;
+
+//data types in js
+// number => 2 to power 53
+// boolean => true/false
+// bigint 
+// null => standalone 
+// undefined
+//symbol => for uniqueness
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
+
+//typeof tells about the type of the data-type
---
+++ js_basics/variables.js
@@ -0,0 +1,8 @@
+const acountId = 12345;
+var emailId = "mohit@gmail.com";
+var acountPass = "1234";
+
+// acountId = 34; not allowed
+// instead of console.log we can use console.table() --> this will print the content in tabular form.
+
+console.table([acountId, emailId, acountPass]);
