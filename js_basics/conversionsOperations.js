31694249a491f514f32f1fca8fc358e8264979e7
mohit-aglawe <154004573+mohit0709@users.noreply.github.com>
2024-04-10 10:45:14 +0000

    converting types

---
+++ js_basics/conversionsOperations.js
@@ -0,0 +1,30 @@
+// let score = "33abc"
+
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); // output for this is NaN (not a number), we typescript instead
+
+/* 
+conversions
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+
+*/
+
+// let isLoggedIn = 1;
+// let convertIsLoggedIn = Boolean(isLoggedIn);
+// console.log(convertIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "something" => true
+
+// string conversion
+
+let num = 33;
+let stringNumber = String(num);
+console.log(typeof(stringNumber));
+console.log("hi");
